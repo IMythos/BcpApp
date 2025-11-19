d063387152c741ea6ac6e8c6938b9883d8b926b7
mythos <developerwebmythos@gmail.com>
2025-11-19 17:05:29 -0500

    EF001 - Initialize project, modules and packages structure. Dependencies installed.

---
+++ src/app/features/client/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'client-page',
+  standalone: true,
+  imports: [],
+  templateUrl: 'home.component.html'
+})
+
+export class HomeComponent {
+
+}
